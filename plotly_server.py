cd2d455384fe8c701300bc9b6561b5d2c53adaf5
kano <ajvls98@gmail.com>
2023-10-16 04:18:16 +0900

    d

--- plotly_server.py
+++ plotly_server.py
@@ -11,7 +11,7 @@ from config import MONGO_URI
 
 # MongoDB 연결
 
-client = MongoClient(MONGO_URI, server_api=ServerApi('1'))
+client = MongoClient("mongodb+srv://mafumafu9854:3eWoSwhmDvhlim9L@cluster0.nxdfqvk.mongodb.net/?retryWrites=true&w=majority", server_api=ServerApi('1'))
 
 try:
     client.admin.command('ping')
